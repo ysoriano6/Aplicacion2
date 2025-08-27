394e2307ffeadae6dcbcf8a585866d2e177acf3b
ysoriano6 <yerik.soriano@gmail.com>
2025-08-27 15:30:07 -0600

    Se crearon dos botones

--- Aplicacion2/Form1.Designer.vb
+++ Aplicacion2/Form1.Designer.vb
@@ -22,16 +22,41 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Button1 = New Button()
+        Button2 = New Button()
         SuspendLayout()
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(95, 73)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(163, 50)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(100, 148)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(158, 45)
+        Button2.TabIndex = 1
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(800, 450)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
         Name = "Form1"
         Text = "Proyecto2"
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
 
 End Class
